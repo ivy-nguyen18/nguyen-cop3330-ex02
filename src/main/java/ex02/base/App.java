8db586eab441f481a7245a8593277dd3143e08c8
ivy-nguyen18 <nguyen.ivy1802@knights.ucf.edu>
2021-06-05 01:30:39 -0400

    Initial commit

---
+++ src/main/java/ex02/base/App.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Ivy Nguyen
+ */
+
+package ex02.base;
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is the input string?");
+        String word = input.nextLine();
+
+        System.out.printf("%s has %d characters", word, word.length());
+    }
+}
